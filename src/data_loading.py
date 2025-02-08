fff34c67ae5b997d68d93e689b8c13f0c11da5e7
Sanjay <sanjaymvkrishna@example.com>
2025-02-08 21:45:21 +0530

    update data loading functions to use absolute paths for dataset files

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 def load_data():
-    df = pd.read_csv(r'data\Disease_Prediction_Dataset.csv')
+    df = pd.read_csv(r'D:\DATA_SCIENCE\PROJECTS\ML Disease Diagnosis\data\Disease_Prediction_Dataset.csv')
     return df
 
 def load_traindata():
-    df = pd.read_csv(r'data\Training.csv')
+    df = pd.read_csv(r'D:\DATA_SCIENCE\PROJECTS\ML Disease Diagnosis\data\Training.csv')
     return df
 
 def load_testdata():
-    df = pd.read_csv(r'data\Testing.csv')
+    df = pd.read_csv(r'D:\DATA_SCIENCE\PROJECTS\ML Disease Diagnosis\data\Testing.csv')
     return df
 
